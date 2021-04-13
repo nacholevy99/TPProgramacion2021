bddb3e0b2e5233365bf663c1203abc49cf761fc5
nacholevy99 <juanignaciolevy@gmail.com>
2021-04-13 16:31:47 -0300

    solucion nodemon

--- ProyectoMascotas/app.js
+++ ProyectoMascotas/app.js
@@ -6,7 +6,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
-let loginRouter = require('./routes/login');
+// let loginRouter = require('./routes/login'); tengo q ver esto despues
 
 let app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
+// app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
