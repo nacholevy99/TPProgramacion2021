99701891f522536d087a9de543cb29572baa8fcf
nacholevy99 <juanignaciolevy@gmail.com>
2021-04-13 17:39:19 -0300

    muevo partials a views

--- ProyectoMascotas/routes/ingeso.js
+++ ProyectoMascotas/routes/ingeso.js
@@ -1,9 +1,10 @@
 let express = require('express')
 let router = express.Router();
 
+
 router.get('/register', function(req, res, next) {
-    res.render('REGISTRO');
+    res.send('REGISTRO');
   });
 
-
+  
 module.exports = router;
