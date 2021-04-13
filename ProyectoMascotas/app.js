108a80a49db1c9b3a9af7b08ccb4cf1215d46d6b
nacholevy99 <juanignaciolevy@gmail.com>
2021-04-13 17:49:46 -0300

    ruta register

--- ProyectoMascotas/app.js
+++ ProyectoMascotas/app.js
@@ -6,7 +6,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
-// let registerRouter = require('./routes/ingreso');
+let registerRouter = require('./routes/ingreso');
 
 let app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/register', registerRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ProyectoMascotas/routes/ingreso.js
+++ ProyectoMascotas/routes/ingreso.js
@@ -1,10 +1,10 @@
 let express = require('express')
 let router = express.Router();
 
-
-router.get('/register', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.send('REGISTRO');
   });
+
 
   
 module.exports = router;
