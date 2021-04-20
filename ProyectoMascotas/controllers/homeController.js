8c6571a1f10777c9b083a036e1c678709d404217
santiagolemoinee <santiagolemoine@gmail.com>
2021-04-20 15:23:14 -0300

    algo de css y controladores

--- ProyectoMascotas/controllers/homeController.js
+++ ProyectoMascotas/controllers/homeController.js
@@ -6,14 +6,16 @@ const homeController = {
 
     
     index: (req,res) => {
-        let listaproductos = productos.lista
-        let respuesta = {lista : listaproductos}
-        for (let index = 0; index < respuesta.lista; index++){
-            console.log(respuesta.lista[index].describirse())
-        }
-        res.render('home', respuesta)},
         
+        let resultado = {
+        nombre: 'pitbul',
+        imagen: '/stylesheets/imagenes/products/pitbull-1.jpg',
+        comentario: 'El American Pit Bull Terrier es una raza canina de Estados Unidos que se originó con la llegada de los inmigrantes ingleses que llevaban el Staffordshire bull terrier,'
+        };
+        
+        res.render('home', resultado)},
     
+
     detalle: (req,res) => 
         res.render('detalle'),
 
