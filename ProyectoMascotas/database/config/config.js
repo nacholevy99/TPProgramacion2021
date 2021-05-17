56bafc363fa9025e292afd6f615433992242b34d
santiagolemoinee <santiagolemoine@gmail.com>
2021-05-17 10:13:13 -0300

    detalles de sequelize

--- ProyectoMascotas/database/config/config.js
+++ ProyectoMascotas/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "IntegradorProgramacion",
+    "database": "integradorprogramacion",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
